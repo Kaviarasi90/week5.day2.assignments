71dd476c86ef0c194a46c6e125e0fc777ddf7ad1
KAVIARASI JAYABALAN <101966808+Kaviarasi90@users.noreply.github.com>
2022-05-25 22:11:35 +0530

    Add files via upload

---
+++ ProjectSpecificMethods.java
@@ -0,0 +1,42 @@
+package week5.day1;
+
+import java.time.Duration;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ProjectSpecificMethods {
+	public RemoteWebDriver driver;
+	
+	@Parameters({"URL","browserName"})
+	@BeforeMethod
+	public void StartBrowser(String url,String browserName) {
+		if(browserName.equals("chrome")){
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		}
+		else if(browserName.equals("edge")){
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		}
+		driver.manage().window().maximize();
+		driver.get(url);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		
+	}
+	@AfterMethod
+	public void EndBrowser() {
+		//driver.close();
+	}
+	
+	
+	
+
+
+}
